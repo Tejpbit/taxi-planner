df7118fde7473851856355f47f82d28700ef3ae4
Johan Lindskogen <johan.lindskogen@gmail.com>
2018-05-05 12:10:28 +0200

    Geocode addresses to markers

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { getData } from "./lib/spreadsheet";
-import MapView from './MapView';
-import "dotenv"
+import MapView from "./MapView";
+import "dotenv";
 
 class App extends Component {
   state = {
@@ -25,12 +25,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        {addresses.map(addr => (
-          <div key={addr.name}>
-            {addr.name}, {addr.street}, {addr.area}
-          </div>
-        ))}
-        <MapView></MapView>
+        <MapView addresses={addresses} />
       </div>
     );
   }
--- src/MapView.js
+++ src/MapView.js
@@ -1,93 +1,129 @@
-import React, { Component } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import React, { Component } from "react";
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 import _ from "lodash";
-import kmeans  from 'node-kmeans';
+import kmeans from "node-kmeans";
+import { getAddress } from "./lib/google-geocode";
 
-const vectors = [[57.7089355,11.9669514],[57.6877847,11.9530877], [57.7041652,11.9636228], [57.6827079,11.9556896], [57.6783956,11.9470382]];
-
-const tilesPaths = [
-  require('./tiles/tiles000.png'),
-  require('./tiles/tiles001.png'),
-  require('./tiles/tiles002.png'),
-  require('./tiles/tiles003.png'),
-  require('./tiles/tiles004.png'),
-  require('./tiles/tiles005.png'),
-  require('./tiles/tiles006.png'),
-  require('./tiles/tiles007.png'),
-  require('./tiles/tiles008.png'),
-  require('./tiles/tiles009.png'),
-  require('./tiles/tiles010.png'),
-  require('./tiles/tiles011.png'),
+const vectors = [
+  [57.7089355, 11.9669514],
+  [57.6877847, 11.9530877],
+  [57.7041652, 11.9636228],
+  [57.6827079, 11.9556896],
+  [57.6783956, 11.9470382]
 ];
 
+const tilesPaths = [
+  require("./tiles/tiles000.png"),
+  require("./tiles/tiles001.png"),
+  require("./tiles/tiles002.png"),
+  require("./tiles/tiles003.png"),
+  require("./tiles/tiles004.png"),
+  require("./tiles/tiles005.png"),
+  require("./tiles/tiles006.png"),
+  require("./tiles/tiles007.png"),
+  require("./tiles/tiles008.png"),
+  require("./tiles/tiles009.png"),
+  require("./tiles/tiles010.png"),
+  require("./tiles/tiles011.png")
+];
 
 class MapView extends Component {
-
   state = {
-    clusters: []
+    clusters: [],
+    latlngs: []
   };
 
-  onMapClicked = (e) => {
-    kmeans.clusterize(vectors, {k: 2}, (err,res) => {
+  onMapClicked = e => {
+    kmeans.clusterize(vectors, { k: 2 }, (err, res) => {
       if (err) console.error(err);
       else {
         this.setState({
           clusters: res
         });
-        console.log('%o',res);
+        console.log("%o", res);
       }
     });
   };
 
-  onMarkerClicked = (e) => {
+  onMapClicked = e => {
+    kmeans.clusterize(vectors, { k: 2 }, (err, res) => {
+      if (err) console.error(err);
+      else {
+        this.setState({
+          clusters: res
+        });
+        console.log("%o", res);
+      }
+    });
+  };
+
+  onMarkerClicked = e => {
     console.log(e);
   };
 
+  async componentDidUpdate(prevProps, prevState) {
+    const { google, addresses } = this.props;
+
+    if (addresses !== prevProps.addresses) {
+      const latlngs = await Promise.all(
+        _.take(addresses, 10).map(addr => getAddress(google, addr))
+      );
+      this.setState({
+        latlngs
+      });
+    }
+  }
+
   render() {
-    console.log(this.state.clusters);
+    const { clusters, latlngs } = this.state;
+    const { google } = this.props;
+    console.log(clusters);
 
-    const markerData = _.flatMap(this.state.clusters, (cluster, i) =>
-      cluster.cluster.map((coordPair, i2) => <Marker
-        key={`${i}${i2}`}
-        title={`${i}`}
-        position={{lat: coordPair[0], lng: coordPair[1]}}
-        icon={{
-          url: tilesPaths[i],
-          scaledSize: new window.google.maps.Size(32,40)
-        }}/>)
+    const markerData = _.flatMap(clusters, (cluster, i) =>
+      cluster.cluster.map((coordPair, i2) => (
+        <Marker
+          key={`${i}${i2}`}
+          title={`${i}`}
+          position={{ lat: coordPair[0], lng: coordPair[1] }}
+          icon={{
+            url: tilesPaths[i],
+            scaledSize: new google.maps.Size(32, 40)
+          }}
+        />
+      ))
     );
-
 
     console.log("marker data: ", markerData);
 
     return (
       <div>
-        <Map google={this.props.google}
-             zoom={14}
-             onClick={this.onMapClicked}
-             initialCenter={{
-          lat: 57.7089355,
-          lng: 11.9669514
-        }}>
+        <Map
+          google={google}
+          zoom={14}
+          onClick={this.onMapClicked}
+          initialCenter={{
+            lat: 57.7089355,
+            lng: 11.9669514
+          }}
+        >
           {markerData}
-
+          {latlngs.map(latlng => (
+            <Marker key={latlng.place_id} position={latlng.geometry.location} />
+          ))}
         </Map>
-        <InfoWindow onClose={this.onInfoWindowClose}
-                    visible={true}>
+        <InfoWindow onClose={this.onInfoWindowClose} visible={true}>
           <div>
             <h1>helo</h1>
           </div>
         </InfoWindow>
-
       </div>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: (process.env.REACT_APP_GOOGLE_MAPS_KEY)
-})(MapView)
-
+  apiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY
+})(MapView);
 
 /*
 

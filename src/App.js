14d50e0e96263c2470cbfa9479f43c3ce7c41d60
Johan Lindskogen <johan.lindskogen@gmail.com>
2018-05-05 10:39:00 +0200

    Fetch addresses from google sheets

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getData } from "./lib/spreadsheet";
 
 class App extends Component {
+  state = {
+    addresses: []
+  };
+
+  async componentDidMount() {
+    const addresses = await getData();
+    this.setState({
+      addresses
+    });
+  }
   render() {
+    const { addresses } = this.state;
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {addresses.map(addr => (
+          <div key={addr.name}>
+            {addr.name}, {addr.street}, {addr.area}
+          </div>
+        ))}
       </div>
     );
   }
